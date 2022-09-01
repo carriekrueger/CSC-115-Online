40ebde6c5a8a5cec6bb983297cddcc0a3414b518
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-09-01 09:07:56 -0400

    Update Ch2Comments.java

--- Ch2Comments.java
+++ Ch2Comments.java
@@ -1,70 +1,61 @@
 // Carrie Krueger
-// 9-6-20
-// Chapter 2 Notes - Comments 
+// 9-1-22
+// Chapter 2 Notes: Comments
 
 public class Ch2Comments {
     public static void main(String[] args) {
-        comments();  // in this method we will explore comments
+        comments(); //in this method we will explore comments
     }
-    
-    // a method to investigate comments
+
+    //a method to investigate comments
     public static void comments() {
-        
-        // anything you type in a comment is NOT read by the computer
-        
+        // anything you write in a comment is NOT read by the computer
+
         // We use comments for:
-        // - Adding your name to the program 
-        // - Adding a brief description of the program 
-        // - As you go along, explain to others (and yourself!) 
-        // what your code should do 
-        
-        // Also... DEBUGGING 
-        // If there is code that doesn't quite work (yet...)
-        // If there are a bunch of lines of code that don't work
-        // and you are trying to isolate where there is an error 
-        
-        
-        // GIVE CREDIT 
-        // If you use someone else's code, use comments to credit them and 
-        // include the place where you got it from
-        
-        // ex. shamelessly stolen from https://github.com/awesomecoder 
-        
-        
+        // -Adding your name to the program
+        // -Adding a brief description of the program 
+        // -As you go along, to explain to others (and yourself) what your code should do
+
+        // Also - DEBUGGING
+        // If there is code that doesn't work quite yet 
+        // If there's a bunch of lines that don't work and you want to isolate them
+
+        // GIVE CREDIT
+        // If you use someone else's code, use comments to credit them/the source
+        // ex. shamelessy stolen from https://github.com/awesomecoder
+
         // HOW DO WE MAKE COMMENTS?
-        
-        // Line comments: anything after the // is ignored by the compiler 
-        // Block comments: /*   */  (slash star star slash)(comment sandwich)
-        // Java Doc comments: /**  */ (will automatically generate code documentation
-        // we won't use these just yet
-        
-        // Examples:
-        
-        /* When you have multiple lines of comments
-           it's helpful to do a block comment because 
-           you can add as many lines as you want. 
-           Good for program name/description at the top of the program.
+
+        // Line comments: anything after the // is ignored by the compiler
+        // Block comments:  /* */  (slash star star slash) (comment sandwich)
+        // Java Doc comments: /** */ will automatically generate code documentation
+
+        // EXAMPLES
+
+        /*  When you have multiple lines of comments
+            it's helpful to do a block comment because
+            you can add as many lines as you want.
+            Good for program name/description at the top of the program
         */
-        
-        // Debugging: 
-        
+
+        // DEBUGGING
+
+        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
         System.out.println("Hello World!");
         //System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+
+
         /*
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-        */
         System.out.println("Hello World!");
-        
-           
-        
-        
-        
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        */
+
     }
 }
-© 2021 GitHub, Inc.
