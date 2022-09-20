0363404d185dd1c3061851128abf7411065b193b
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-09-20 14:33:29 -0400

    Update MoreIfs.java

--- MoreIfs.java
+++ MoreIfs.java
@@ -1,131 +1,123 @@
 // Carrie Krueger
-// 3-4-21
-// Efficiently using if-statements and multiple conditionals
+// 9-15-22
+// Efficiently using if/else statements and multiple conditionals
 
 import java.util.Scanner;
 
 public class MoreIfs {
     public static void main(String[] args) {
-        
-        betterIfs();  // efficient if statements
-        multiples();  // a look at multiple conditionals
+
+       betterIfs(); // efficiently using if/else statements
+       multipleIfs(); // a look at ways to handle multiple conditionals
     }
-    
-    // a method that shows the preferred way to handle 
-    //code that is being reused
+
+    // This method shows the preferred way to handle code that is being reused
     public static void betterIfs() {
-        
-        // preferred way 
-        
+
+        // When you have code that ends up being reused in your program:
+        // Preferred way:
+
         Scanner sc = new Scanner(System.in);
         System.out.println("What is your age?");
+
         int age = sc.nextInt();
-        
+
         if(age >= 65) {
-             System.out.println("Age is just a number!");
+            System.out.println("Age is just a number!");
         }
-        
+
         System.out.println(age + " is a good age.");
-        
-        
-        
-        // less preferred way
-        
-        /*
-        
+
+
+        /* less preferred way:
+
         Scanner sc = new Scanner(System.in);
         System.out.println("What is your age?");
+
         int age = sc.nextInt();
-        
+
         if(age >= 65) {
             System.out.println("Age is just a number!");
             System.out.println(age + " is a good age.");
         }
-        
+
         if(age < 65) {
             System.out.println(age + " is a good age.");
-            
         }
-        
+
         */
-        
-        
+
+
     }
-    
-    // this method looks at how to do multiple conditionals
-    public static void multiples() {
-        
-        // let's compare a num to 10 in a better way
-        // than we did before
-        // multiple ways to handle multiple conditionals 
-        
-        
+
+    // This method looks at ways to do multiple conditionals and Boolean logic
+    public static void multipleIfs() {
+
+        // let's compare num to 10 in a better way than we did before
+        // multiple ways to handle multiple conditionals
+
         // only one outcome possible
-        // NESTED 
-        // note the indentation-  if/else/ { --> INDENT next line
+        // NESTED
+        // note the indentation:  every {  --> next line indented
         
-        int num = 10;
-        
+        int num = 42;
+
         if(num > 10) {
-            System.out.println("Greater!");
+            System.out.println("num is greater!");
         } else {
             if(num < 10) {
-                System.out.println("Less!");
+                System.out.println("num is less!");
             } else {
-                System.out.println("Equal!");
+                System.out.println("num is equal!");
             }
         }
-        
+
+
         // only one outcome possible
-        // nesting neater... else ifs 
-        
+        // neater nesting
         if(num > 10) {
-            System.out.println("Greater!");
+            System.out.println("num is greater!");
         } else if(num < 10) {
-            System.out.println("Less!");
+            System.out.println("num is less!");
         } else {
-            System.out.println("Equal!");
+            System.out.println("num is equal!");
         }
-        
-        // more than one outcome possible 
-        // NOT nested
-        
+
+
+        // more than one outcome possible
+        // not nested
+
         num = 101;
-        
+
         if(num > 10) {
-            System.out.println("Greater!");
-        } 
+            System.out.println("num is greater!");
+        }
         if(num > 100) {
-            System.out.println("Really big number!");
+            System.out.println("really big number!");
         }
-        
-        // boolean logic operators: and/or 
-        
-        num = 32; 
-        
-        // OR
-        if(num >= 40 || num <= 10) {
-            System.out.println("You win!!!");
-        }
-        
-        // AND ** short circuit **
+
+
+        num = 42;
+
+        // boolean logic operator AND --> short-circuit
         if(num >= 40 && num <= 60) {
             System.out.println("num is in range!");
         }
-        
-        
-        /* 
-        Another option: SWITCH STATEMENTS 
-        Textbook 6.3 
-        Pros and Cons:
-        -if/else can handle ranges
-        -switch statements can only handle exact values
-        -switch statements can only be used with certain primitives and Strings
-        -if/else can get messy with { }, more prone to errors
-        -(syntax or logic)
-        */
-        
+
+        // boolean logic operator OR   --> short-circuit
+        if(num >= 40 || num <= 10) {
+            System.out.println("You win!");
+        }
 
     }
-}
 
+    // Another option: SWITCH STATEMENTS
+    // Textbook 6.3
+
+    // Pros and Cons:
+    // -if/else statements can handle ranges
+    // -switch statements can only handle exact values
+    // -switch statements can only be used with certain primitives and String
+    // -if/else statements can get messy with { }, more prone to errors
+    // (both syntax and logic)
+}
