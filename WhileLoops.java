6f6398372714f0325baf121d0c27c52fd1b04ff3
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-10-06 15:29:24 -0400

    Update WhileLoops.java

--- WhileLoops.java
+++ WhileLoops.java
@@ -1,65 +1,65 @@
 // Carrie Krueger
-// 3-18-21
-// All about while loops 
-
+// 10-06-22
+// All about while loops
 
 public class WhileLoops {
     public static void main(String[] args) {
-        
+
        loops(); // while loops
     }
-    
-    // this method looks at while loops 
+
+    // this method looks at while loops
     public static void loops() {
-        
-        // Ask a question once: if statement 
-        // Ask a question repeatedly (or do something repeatedly):
-        // LOOP (while loop)
-        
+
+        // Ask a question one time: if statement
+        // Ask a question repeatedly (or do something repeatedly): loop (while loop)
+
         // LOOP RULES:
-        
-        // When you program a loop, you should know that it will 
-        // DEFINITELY end once some condition has occurred
-        
-        // Usually you will have to increment some variable
+
+        // When you program a loop, you should know that it will
+        // DEFINITELY end once some condition has occurred 
+
+        // Usually you have to increment some variable
         // so that the loop will approach the end state
         // LCV (loop control variable)
-        
+
         // WAYS TO INCREMENT A VARIABLE (3.7 in textbook)
-        
-        int count = 0; 
-        // How to add 1?
-        count = count + 1; //adds 1 and assigns the value to count
-        count += 1; // compound opertor (shortcut)
-        count++; // another common way to increment by 1 
-        
-        // Use curly brackets { } around the while loop code!! 
-        
-        // HERE COMES A LOOP! 
-        
-        int counter = 0; 
-        
+
+        int count = 0;
+        // how to add 1?
+
+        count = count + 1;  //adds 1 and saves the updated value (assigns) to count
+        count += 1;  // compound operator (shortcut)
+        count++;  //another common way to increment by 1
+
+        // One more note: use curly braces {  } around the while loop code!
+
+        // HERE COMES A LOOP
+
+        int counter = 0;
+
         // ends when the value of counter is 5
+
         while(counter != 5) {
-            counter += 1;   // increment counter to get to 5
             System.out.println(counter);
+            counter += 1;   // increment (get counter to 5)
         }
+
         // how many times does this loop run? 5 times
-        // can adjust start/end/increment values (also use < >)
-        
-        int num = 0; 
-        while(num < 3) {
+        // can adjust the start/end/increment values, can also use <, >, etc.
+
+
+        int num = 0;
+        while (num < 3) {
             System.out.println(num);
         }
-        
-        // INFINITE LOOP OF DOOM!
-        // needs an incrementor 
-        // Exit/break out of the terminal : Ctrl + c
-        
-        // LIVE CODE DEMOS IN BLACKBOARD
-        
-        
-       
-        
+
+        // INFINITE LOOP OF DOOM (needs an incrementor)
+        // Ctrl+c to escape/break the loop in the terminal?
+
+        // Live Code Demos in Brightspace
+
+
     }
+
 }
