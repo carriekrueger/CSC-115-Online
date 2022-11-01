0b6e5cfde28595a04a8d81bab6257aeb21753ad9
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2022-11-01 11:57:05 -0400

    Update PasswordValidate.java

--- PasswordValidate.java
+++ PasswordValidate.java
@@ -1,7 +1,6 @@
 // Carrie Krueger
-// 4-8-21
+// 11-1-22
 // Partial code for validatePassword() method from Project: Password Suite
-
 
 public class PasswordValidateNotes {
 	
@@ -20,48 +19,52 @@ public class PasswordValidateNotes {
 	// this method takes a String password and returns true if the password
 	// is valid according to the rules
 	public static boolean validatePassword(String input) {
-	    
-	    // ex. input = "ABCdef123!"
-	    
-	    // Rules: at least 8 characters, 
-	    //        at least 1 capital letter,
-	    //        at least 2 numbers
-	    
-	    
-	    // check length for 8 characters
-	    if(input.length() < 8) {
-	        return false;
-	    }
-	    
-	    // check for at least 1 capital letter
-	    
-	    int loopCounter = 0; 
-	    int capitalCounter = 0;
-	    
-	    while(loopCounter < input.length())  {
-	        char ch = input.charAt(loopCounter);
-	        
-	        if(ch >= 'A' && ch <= 'Z') {
-	            capitalCounter++;
-	        }
-	        
-	        
-	        loopCounter++;
-	        
-	    }
-	    
-	    // check for at least 2 numbers 
-	    
-	   // *** WRITE THIS CODE ****
-	    
-	    
-	   // returns true if password passes the above tests
-	   if(capitalCounter >= 1 && numberCounter >= 2) {
-	       return true;
-	   } else {
-	       return false;
-	   }
-	    
-	    
-	}
+
+        // ex. input = "ABcDEfg123!"
+
+        // Rules:  
+        // at least 8 characters
+        // at least 1 capital letter
+        // at least 2 numbers 
+
+        // check length for 8 characters
+
+        if(input.length() < 8) {
+            return false;
+        }
+
+        // check for at least 1 capital letter
+
+        int loopCounter = 0;
+        int capitalCounter = 0;
+
+        while(loopCounter < input.length()) {
+
+            char ch = input.charAt(loopCounter);
+
+            if(ch >= 'A' && ch <= 'Z') {
+                capitalCounter++;
+            }
+
+            loopCounter++;
+        }
+
+        // check for at least 2 numbers
+
+        // *** WRITE THIS PART OF THE CODE *** //
+
+        
+        
+
+        // returns true if password passes the above tests
+
+        if(capitalCounter >= 1 && numberCounter >= 2) {
+            return true;
+        } 
+        return false;
+
+        
+
+    }
+
 }
